f270a5896dfa8b0539a386dc0b50cc0817f8d478
omarsd03 <34853959+omarsd03@users.noreply.github.com>
2018-04-12 19:59:37 -0500

    Calcular Total

--- php/calcular.php
+++ php/calcular.php
@@ -3,6 +3,7 @@
     $producto = $_POST['producto'];
     $precio = $_POST['precio'];
     $cantidad = $_POST['cantidad'];
+    $total = $precio * $cantidad;
 
 ?>
 
@@ -16,9 +17,10 @@
     <title>Factura</title>
 </head>
 <body>
-    <h1>Total a pagar:</h1>
+    <h1>Factura</h1>
     <p>Precio: <?php echo $producto; ?> </p>
     <p>Producto: <?php echo $precio; ?> </p>
     <p>Cantidad: <?php echo $cantidad; ?> </p>
+    <p>Total: <?php echo $total; ?> </p>
 </body>
 </html>
