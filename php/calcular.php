af0446069db7f73bd9a5b81248f4128b1baa5bc5
omarsd03 <34853959+omarsd03@users.noreply.github.com>
2018-04-12 19:42:25 -0500

    Agregar HTML

--- php/calcular.php
+++ php/calcular.php
@@ -1,5 +1,24 @@
 <?php 
 
     $producto = $_POST['producto'];
+    $precio = $_POST['precio'];
+    $cantidad = $_POST['cantidad'];
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/estilos.css">
+    <title>Factura</title>
+</head>
+<body>
+    <h1>Total a pagar:</h1>
+    <p></p>
+    <p></p>
+    <p></p>
+</body>
+</html>
